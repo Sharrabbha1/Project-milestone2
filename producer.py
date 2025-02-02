dbec6cfdd32eb71f6e4b1d2806802a45d1dea7f6
Suryateja Gorthi <115051132+Sharrabbha1@users.noreply.github.com>
2025-02-01 21:38:54 -0500

    Update producer.py

--- producer.py
+++ producer.py
@@ -4,7 +4,6 @@ import json
 import os
 import csv
 
-#Search for the JSON service account key
 files = glob.glob("*.json")
 if files:
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = files[0]
@@ -12,19 +11,15 @@ else:
     print("❌ No service account JSON found!")
     exit()
 
-#Set the project_id with your project ID
 project_id = "spheric-mission-448720-i7"
 topic_name = "csvTopic"
 
-#Create a publisher and get the topic path for the publisher
 publisher = pubsub_v1.PublisherClient()
 topic_path = publisher.topic_path(project_id, topic_name)
 print(f"Publishing messages to {topic_path}.")
 
-#Path to the CSV file
 file_path = "Labels.csv"
 
-#✅ Function to Convert Data Types from CSV
 def convert_value(value):
     """Convert CSV values to int, float, or keep as string."""
     try:
@@ -34,15 +29,12 @@ def convert_value(value):
     except ValueError:
         return value  # If conversion fails, return as string
 
-#✅ Open and Read the CSV File
 with open(file_path, mode='r') as csv_file:
     reader = csv.DictReader(csv_file)
 
     for row in reader:
-        # Convert each value dynamically
         converted_row = {key: convert_value(value) for key, value in row.items()}
 
-        # Serialize the message correctly
         message = json.dumps(converted_row).encode('utf-8')
 
         #send the value
